431792dc2399a30fc452a4feed66030bb1714b67
ashir7171 <u2213256@uel.ac.uk>
2024-05-16 04:06:31 +0100

    Basic speed converter

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,3 +1,48 @@
 public class SpeedConverter
 {
+
+    public static void main(String[] args)
+    {
+       long v=  toMilesPerHour(1.5);
+        System.out.println(v );
+
+    }
+
+    public static long toMilesPerHour(double KilometersPerHour){
+
+        double miles = 0;
+        if(KilometersPerHour < 0 ){
+            return -1 ;
+        }else{
+
+            miles =  KilometersPerHour/1.609;
+
+            return Math.round(miles);
+
+
+
+        }
+    }
+
+    public static void printConversion(double KilometersPerHour)
+    {
+
+        if(KilometersPerHour <0){
+            System.out.println("Invalid Value");
+        }else{
+            double miles = KilometersPerHour/1.609;
+
+            System.out.println(KilometersPerHour + " km/h = " + Math.round(miles)+" mi/h");
+        }
+
+    }
+
+
+
+
+
+
+
+
 }
+
